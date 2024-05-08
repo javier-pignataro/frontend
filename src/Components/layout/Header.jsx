e3761891b8bddaa5a30ed2454071ae76ed836d82
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2024-05-08 20:05:01 -0300

    Cambios en estilo / maquetado

--- src/Components/layout/Header.jsx
+++ src/Components/layout/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../../assets/logo-dark-transparente.png'
+import logoLight from '../../assets/logo-light-transparente.png'
+ 
 
 const Header = () => {
     return (
         <nav>
-            <Link to='/'><img src={logo}></img> ArrancAR</Link>
+            <Link to='/'><img src={logoLight}></img> ArrancAR</Link>
             <div className='navbar__buttons'>
                 <Link><button>Crear Cuenta</button></Link>
                 <Link><button>Iniciar Sesión</button></Link>
