59532424dede6002c05ba8c9e0326cc707dffab6
Andy <andy@lancito01.dev>
2024-05-08 22:18:32 -0300

    agregado espaciado innecesario

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,13 +3,11 @@ import { Link } from "react-router-dom";
 const Card = ({ car: selectedCar }) => {
     return (
         <Link to={`/cars/${selectedCar.id}`}>
-            <div className="selected__car__detail__container">
-                <div className='car__card' id={selectedCar.id}>
-                    <img src={selectedCar.images[0]} alt={selectedCar.name} />
-                    <h4>{selectedCar.brand} {selectedCar.model}</h4>
-                    <p>{selectedCar.description}</p>
-                    <p>${selectedCar.price} ARS</p>
-                </div>
+            <div className='car__card' id={selectedCar.id}>
+                <img src={selectedCar.images[0]} alt={selectedCar.name} />
+                <h4>{selectedCar.brand} {selectedCar.model}</h4>
+                <p>{selectedCar.description}</p>
+                <p>${selectedCar.price} ARS</p>
             </div>
         </Link>
     );
